7efbf836b546ba122a9b94a8a6c3917e0788c617
Myra Zmarsly <myra@MacBook-Pro-von-Myra.local>
2022-04-20 10:35:54 +0200

    Change layers

--- model/model_utils.py
+++ model/model_utils.py
@@ -13,12 +13,11 @@ class RegressionHead(nn.Module):
     def __init__(self, dropout=0.2, D_in=768, D_hidden1=100, D_hidden2=10, D_out=1):
         super(RegressionHead, self).__init__()
         self.bert_head = nn.Sequential(
-            nn.Dropout(0.5),
+            nn.Dropout(0.2),
             nn.Linear(D_in, D_hidden1),
 	        nn.ReLU())
 
         self.regressor = nn.Sequential(
-            nn.Dropout(0.2),
             nn.Linear(D_hidden1, D_hidden2),
 	        nn.ReLU(),
             nn.Dropout(0.1),
