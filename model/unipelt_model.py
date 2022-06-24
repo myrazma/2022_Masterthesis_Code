fdb8971c5f2e1348448d9b43a3634654ba8fc89c
Myra Zmarsly <myra.zmarsly@stud.tu-darmstadt.de>
2022-06-24 14:11:38 +0200

    Only config to bert for sequence classification

--- model/unipelt_model.py
+++ model/unipelt_model.py
@@ -590,7 +590,7 @@ def main():
     ###)
 
     model = BertForSequenceClassification(  # leading to the exact same result as oru classification!
-        model_args.model_name_or_path,
+        #emodel_args.model_name_or_path,
         #from_tf=bool(".ckpt" in model_args.model_name_or_path),
         config=config,
         #cache_dir=model_args.cache_dir,
