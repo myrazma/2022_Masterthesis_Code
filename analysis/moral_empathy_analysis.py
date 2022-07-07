d91094ec65f21e8d4fe57464b74b299ee6aa5bb0
Myra Zmarsly <myra.zmarsly@stud.tu-darmstadt.de>
2022-07-07 10:34:52 +0200

    Add data and Dockerfile for MoRT (Moral Dimension)

---
+++ analysis/moral_empathy_analysis.py
@@ -0,0 +1,24 @@
+import pickle
+import sklearn
+print (sklearn.__version__)
+from sklearn.decomposition import PCA
+
+
+
+file_path = '../data/MoRT_projection/projection_model.p'
+file = open(file_path, 'rb')
+
+# dump information to that file
+data = pickle.load(file)
+
+# close the file
+file.close()
+
+print('Showing the pickled data:')
+
+cnt = 0
+for item in data:
+    print('The data ', cnt, ' is : ', item)
+    cnt += 1
+
+print(type(data))
