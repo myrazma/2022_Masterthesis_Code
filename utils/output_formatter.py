27bf1ed49016a41a197b68194c9457cc3b6f4339
Myra Zmarsly <myra.zmarsly@stud.tu-darmstadt.de>
2022-09-09 18:37:16 +0200

    Update output formatter script for Codalab

--- utils/output_formatter.py
+++ utils/output_formatter.py
@@ -2,12 +2,13 @@
 Module for output formatter to upload predictions to codalab EMP competiton    
 """
 
+from ast import arg
 import sys
 import os
 import pandas as pd
 
 
-def generate_result_file(model_name, output_dir='../output/', store_as_tsv=True, columns=['empathy', 'distress'], output_file_name='predictions_EMP'):
+def generate_result_file(model_name, output_dir='../output/', store_as_tsv=True, columns=['empathy', 'distress'], output_file_name='predictions_EMP', split='test'):
     """Loading from results file of a model and generate an output file
 
     Args:
@@ -23,7 +24,7 @@ def generate_result_file(model_name, output_dir='../output/', store_as_tsv=True,
     # check which subtasks_are available
     output_task_paths = {subdir.split('/')[-1]: subdir for subdir in subdirs}
 
-    test_result_file_name = 'test_results'
+    test_result_file_name = f'{split}_results'
     results_df = pd.DataFrame()
     for col in columns:
         if col in output_task_paths.keys():
@@ -48,9 +49,23 @@ if __name__ == "__main__":
         sys.exit(-1)
     
     args = sys.argv[1:]  # ignore first argument (name of file)
+    split_arg = 'test'  # as base if not other defined
+    if '--split' in args:
+        split_arg_idx = int(args.index('--split') + 1)
+        if split_arg_idx < len(args):
+            split_arg = args[split_arg_idx]
+        args.pop(split_arg_idx - 1)
+        args.pop(split_arg_idx -1)
+
+    if split_arg == 'test':
+        output_file_name='predictions_EMP'
+    else:
+        output_file_name=f'predictions_EMP_{split_arg}'
+
+
     for model_name in args:
         try:
-            generate_result_file(model_name, store_as_tsv=True)
+            generate_result_file(model_name, store_as_tsv=True, split=split_arg, output_file_name=output_file_name)
         except Exception as e:
             print(f"MyWarning: Wasn't able to generate output from the following model {model_name}. Exception occured:\n {e}")
 
